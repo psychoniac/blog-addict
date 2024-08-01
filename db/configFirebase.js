fd2b20820f985c9dea7698e5fce30f14c42e4f7a
jlnko <jlnko@hotmail.fr>
2024-08-01 15:52:56 +0200

    création des variables dans fiorebase config, création du storage et de l'auth dans firebase

--- db/configFirebase.js
+++ db/configFirebase.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,3 +13,6 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
